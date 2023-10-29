bb8537f67d988e77ede81d0efa9fe18f2efb99c5
João Marcos <andramas.10@gmail.com>
2023-10-29 11:50:22 -0300

    Novas Modificações a partir do Diagrama

---
+++ GitHub/src/projeto/ItemControle.java
@@ -0,0 +1,44 @@
+package projeto;
+
+public abstract class ItemControle {
+	protected String nome;
+	protected int id;
+	protected String descricao;
+	
+	public ItemControle(String nome, int id, String descricao) {
+		this.nome = nome;
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + ", Id: " + id + ", Descrição: " + descricao;
+	}
+	
+	
+}
