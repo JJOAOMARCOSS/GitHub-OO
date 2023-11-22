6faa418d60836de1391e21bae834664f28ac666e
Gabriel Lima <gabriel58221@gmail.com>
2023-11-22 19:54:05 -0300

    Apagando métodos não utilizados

--- GitHub/src/projeto/Repositorio.java
+++ GitHub/src/projeto/Repositorio.java
@@ -3,13 +3,10 @@ package projeto;
 public class Repositorio {
 	private String nome;
 	private String dtCriacao;
-	private ItemControle[] listaItensControles = new ItemControle[100]; 
-	private int numItensControles;
-	
+
 	public Repositorio(String nome, String dtCriacao) {
 		this.nome = nome;
 		this.dtCriacao = dtCriacao;
-		numItensControles = 0;
 	}
 
 	public String getNome() {
@@ -28,76 +25,6 @@ public class Repositorio {
 		this.dtCriacao = dtCriacao;
 	}
 
-	public ItemControle[] getListarItensControles() {
-		return this.listaItensControles;
-	}
-
-	public void setListarItensControles(ItemControle[] ic) {
-		this.listaItensControles = ic;
-	}
-
-	public ItemControle getItemControle(int i) {
-		return listaItensControles[i];
-	}
-	
-	public void setItemControle(ItemControle ic, int i) {
-		this.listaItensControles[i] = ic;
-	}
-	
-	public int getNumItensControles() {
-		return numItensControles;
-	}
-
-	public void setNumItensControles(int numItensControles) {
-		this.numItensControles = numItensControles;
-	}
-	/* 
-	public boolean addItemControle(ItemControle ic) {
-		if(numItensControles < 100) {
-			listaItensControles[numItensControles] = ic; 
-			numItensControles++;
-			
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-	public void editarItemControle(int i, ItemControle r) {
-		if(i < getNumItensControles() && i >= 0) {
-			setItemControle(r, i);
-			System.out.println("Dados editados com sucesso");
-		} 
-	}
-
-	public void removerItemControle(String nomeRepositorio) {
-        for (int i = 0; i < numItensControles; i++) {
-            if (listaItensControles[i] != null && listaItensControles[i].getNome().equals(nomeRepositorio)) {
-                // Move os repositórios restantes uma posição para frente
-                for (int j = i; j < numItensControles - 1; j++) {
-                    listaItensControles[j] = listaItensControles[j + 1];
-                }
-                listaItensControles[numItensControles - 1] = null;
-                numItensControles--;
-                System.out.println("Removido com sucesso.");
-                return;
-            }
-        }
-        System.out.println("Não encontrado para remoção.");
-    }
-	
-	public ItemControle buscaItemControle(String nomeItemControle) {
-		ItemControle saida = null;
-		for(int i = 0; i < numItensControles; i++) {
-			if(saida.getNome().compareToIgnoreCase(nomeItemControle) == 0) {
-				saida = listaItensControles[i];
-			}
-		}
-			return saida;
-	}
-	*/
-	
 	public String toString() {
 		return "Nome: " + nome + ", Data de criacao: " + dtCriacao;
 	}
--- GitHub/src/projeto/Usuario.java
+++ GitHub/src/projeto/Usuario.java
@@ -4,14 +4,11 @@ public class Usuario {
 	private String nome;
 	private String email;
 	private String senha;
-	private Repositorio[] listaRepositorios = new Repositorio [100];
-	private int numRepositorios;
 	
 	public Usuario(String nome, String email, String senha) {
 		this.nome = nome;
 		this.email = email;
 		this.senha = senha;
-		numRepositorios = 0;
 	}
 
 	public String getNome() {
@@ -37,97 +34,7 @@ public class Usuario {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-
 	
-	public Repositorio[] getListaRepositorios() {
-		return this.listaRepositorios;
-	}
-
-	public void setArrayListaRepositorios(Repositorio[] r) {
-		this.listaRepositorios = r;
-	}
-	
-	public Repositorio getRepositorio(int i) { 
-		return listaRepositorios[i];
-	}
-	
-	public void setArrayRepositorio(Repositorio repo, int i) {
-		this.listaRepositorios[i] = repo;
-	}
-	
-	public void setRepositorio(Repositorio repo, int i) {
-		this.listaRepositorios[i] = repo;
-	}
-
-	public int getNumRepositorios() {
-		return numRepositorios;
-	}
-
-	public void setNumRepositorios(int numRepositorios) {
-		this.numRepositorios = numRepositorios;
-	}
-	
-	/*public boolean addrepositorio(Repositorio r) {
-		if(numRepositorios < 100) {
-			listaRepositorios[numRepositorios] = r;
-			numRepositorios++;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public Repositorio buscaRepositorio(String nomeRepositorio) {
-
-        Repositorio repositorioAchado = null; //saida
-        Repositorio repositorioParaComparar;
-
-        for(int i = 0; i < numRepositorios; i++) {
-            repositorioParaComparar = listaRepositorios[i];
-            if(repositorioParaComparar.getNome().compareToIgnoreCase(nomeRepositorio) == 0) {
-                repositorioAchado = repositorioParaComparar;
-                break;
-            }
-        }
-
-        return repositorioAchado; //saida
-    }
-	
-	public boolean removerRepositorio(String nomeRepositorio) {
-        for (int i = 0; i < numRepositorios; i++) {
-            if (listaRepositorios[i] != null && listaRepositorios[i].getNome().equals(nomeRepositorio)) {
-                // Move os repositórios restantes uma posição para frente
-                for (int j = i; j < numRepositorios - 1; j++) {
-                    listaRepositorios[j] = listaRepositorios[j + 1];
-                }
-                listaRepositorios[numRepositorios - 1] = null;
-                numRepositorios--;
-                return true;
-            }
-        }
-        return false;
-    }
-
-	public void editarRepositorio(int i, Repositorio r) {
-		if(i < getNumRepositorios() && i >= 0) {
-			setRepositorio(r, i);
-			System.out.println("Dados editados com sucesso");
-		}
-	}
-	
-    /*
-    //Buscar repositorio por data
-
-    public Repositorio buscaData(String dtCriacao) {
-        Repositorio saida = null;
-        for(int i = 0; i < numRepositorios; i++) {
-            if(saida.getNome().compareToIgnoreCase(dtCriacao) == 0) {
-                saida = listaRepositorios[i];
-            }
-        } 
-            return saida;
-        }*/
-		
 	public String toString() {
 		return "Usuario: " + nome + ", Email: " + email + ", Senha: " + senha;
 	}
